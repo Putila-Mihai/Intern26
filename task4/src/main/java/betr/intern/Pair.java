d2f7660aa899d06a4709924f5f4084b31ad1ba38
denisa.caruntupitchtech.ro <caruntudenisa12@gmail.com>
2026-06-25 11:20:01 +0300

    Complete Task 4

---
+++ task4/src/main/java/betr/intern/Pair.java
@@ -0,0 +1,39 @@
+package betr.intern;
+
+public class Pair <T>{
+
+    private T first; //encapsulation, abstraction, SRP
+    private T second;
+
+    public Pair(T first, T second){
+        this.first=first;
+        this.second=second;
+    }
+
+    public void setFirst(T first){
+        this.first=first;
+    }
+
+    public T getFirst(){
+        return this.first;
+    }
+
+    public void setSecond(T second){
+        this.second=second;
+    }
+
+    public T getSecond(){
+        return this.second;
+    }
+
+
+    @Override
+
+    public String toString(){
+        return "Pair(" +first + "," + second + ")";
+    }
+
+
+
+
+}
